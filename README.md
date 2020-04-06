3f069dccca80b964fcb8fbe0476696314a2c8a78
moonou <moonoulong@gmail.com>
2020-04-06 16:27:54 +0800

    feat: add manual search

---
+++ README.md
@@ -0,0 +1,7 @@
+# ʕ •́؈•̀) `acnh-finder`
+
+动物森友会数据查询
+
+[![siri](https://support.apple.com/library/content/dam/edam/applecare/images/en_US/il/ios12-siri-shortcuts-add-to-siri-inline-icon.png)](https://www.icloud.com/shortcuts/27c0b71d67a84934b1bcf02176284f92)
+
+数据来源于[BILIGAME WIKI](https://wiki.biligame.com/dongsen/%E9%A6%96%E9%A1%B5)
---
+++ src/index.ts
@@ -0,0 +1,49 @@
+import Router from '@meowuu/worker-router'
+import manuals from '../data/manual.json'
+
+addEventListener('fetch', event => {
+  event.respondWith(route(event.request))
+})
+
+function route (req: Request) {
+  const route = new Router()
+  route.get('/manual/:name', (req) => {
+    const { name } = req.params
+    const isSiri = !!req.querys.siri
+    const result = manuals.find((item) =>
+      item.name === req.params.name
+    )
+
+    if (result) {
+      if (isSiri) {
+        return {
+          code: 0,
+          data: {
+            message: `你查找的${name}需要${
+              result.materials.map(({ name, count }) => `${name} ${count}个`).join(`\n`)
+            }`,
+            access: result.access
+          }
+        }
+      }
+      return {
+        code: 0,
+        data: result
+      }
+    }
+
+    return new Response(JSON.stringify({
+      code: 404
+    }), {
+      status: 404
+    })
+  })
+
+  route.default(() => {
+    return {
+      code: 0
+    }
+  })
+
+  return route.route(req)
+}
